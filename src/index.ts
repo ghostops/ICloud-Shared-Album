5cf9004227c029f47397e6ee9f1fb087db22e551
Jonathan Maton <jmaton@maton.org>
2024-10-15 09:45:58 -0400

    Fix for http error 330
    adapted fix from @bertrandom project icloud-shared-album-to-flickr  (thank you very much!)

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,23 @@ import { getBaseUrl } from './baseUrl';
 import { getApiResponse, getUrls } from './api';
 import { ICloud } from './types';
 import * as chunk from 'lodash.chunk';
+import { getRedirectedBaseUrl } from './redirectedBaseUrl';
 
 export * from './types';
 
 export const getImages = async (token: string): Promise<ICloud.Response> => {
-  const baseUrl = getBaseUrl(token);
+  const _baseUrl = getBaseUrl(token);
+  // in 2024 Apple began issuing a 330 (redirect), so we need to deal with that:
+  const newBaseUrl = await getRedirectedBaseUrl(_baseUrl, token);
 
-  const apiResponse = await getApiResponse(baseUrl);
+  const apiResponse = await getApiResponse(newBaseUrl);
 
   const chunks = chunk(apiResponse.photoGuids, 25);
 
   let allUrls: Record<string, string> = {};
 
   for (const chunk of chunks) {
-    const urls = await getUrls(baseUrl, chunk);
+    const urls = await getUrls(newBaseUrl, chunk);
     allUrls = Object.assign(allUrls, urls);
   }
 
