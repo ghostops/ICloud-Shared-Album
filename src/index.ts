f37eb180303b803b053bec9d3ca0ce158720a3a1
Ludvig L <ludvig@ghostops.nu>
2024-10-16 16:02:51 +0200

    just a little renaming

--- src/index.ts
+++ src/index.ts
@@ -8,18 +8,18 @@ import { getRedirectedBaseUrl } from './redirectedBaseUrl';
 export * from './types';
 
 export const getImages = async (token: string): Promise<ICloud.Response> => {
-  const _baseUrl = getBaseUrl(token);
+  const baseUrl = getBaseUrl(token);
   // in 2024 Apple began issuing a 330 (redirect), so we need to deal with that:
-  const newBaseUrl = await getRedirectedBaseUrl(_baseUrl, token);
+  const redirectedBaseUrl = await getRedirectedBaseUrl(baseUrl, token);
 
-  const apiResponse = await getApiResponse(newBaseUrl);
+  const apiResponse = await getApiResponse(redirectedBaseUrl);
 
   const chunks = chunk(apiResponse.photoGuids, 25);
 
   let allUrls: Record<string, string> = {};
 
   for (const chunk of chunks) {
-    const urls = await getUrls(newBaseUrl, chunk);
+    const urls = await getUrls(redirectedBaseUrl, chunk);
     allUrls = Object.assign(allUrls, urls);
   }
 
